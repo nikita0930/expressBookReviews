7c93d1b33689ff3889af8f074ad613510f6cc4c1
nikita0930 <nikitagamit30@gmail.com>
2026-04-01 17:22:42 +0530

    Completed Book Review API with JWT authentication and Axios tasks

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
+
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +9,54 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+// Session setup
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// 🔐 Authentication Middleware
+app.use("/customer/auth/*", function auth(req, res, next) {
+  try {
+    // Get token from headers
+    const token = req.headers.authorization;
+
+    // Check if token exists
+    if (!token) {
+      return res.status(401).json({ message: "Access token missing" });
+    }
+
+    // Verify token
+    jwt.verify(token, "fingerprint_customer", (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      }
+
+      // Store user info in session
+      req.session.authorization = {
+        accessToken: token,
+        username: user.username
+      };
+
+      // Continue to next route
+      next();
+    });
+
+  } catch (error) {
+    return res.status(500).json({ message: "Authentication error" });
+  }
 });
- 
-const PORT =5000;
 
+// Routes
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+// Server start
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -6,38 +7,109 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  if (!isValid(username)) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+
+  users.push({ username, password });
+  return res.status(200).json({ message: "User successfully registered. Now you can login" });
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
+});
+
+// Get the book list available in the shop using async-await with Axios
+public_users.get('/asyncbooks', async function (req, res) {
+  try {
+    const response = await axios.get('http://127.0.0.1:5000/');
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to retrieve books using Axios' });
+  }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn], null, 4));
  });
+
+// Get book details based on ISBN using async-await with Axios
+public_users.get('/asyncisbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`http://127.0.0.1:5000/isbn/${isbn}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to retrieve book details using Axios' });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  const authorBooks = {};
+
+  bookKeys.forEach((key) => {
+    if (books[key].author === author) {
+      authorBooks[key] = books[key];
+    }
+  });
+
+  return res.send(JSON.stringify(authorBooks, null, 4));
+});
+
+// Get book details based on author using async-await with Axios
+public_users.get('/asyncauthor/:author', async function (req, res) {
+  try {
+    const author = req.params.author;
+    const response = await axios.get(`http://127.0.0.1:5000/author/${author}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to retrieve author details using Axios' });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const bookKeys = Object.keys(books);
+  const titleBooks = {};
+
+  bookKeys.forEach((key) => {
+    if (books[key].title === title) {
+      titleBooks[key] = books[key];
+    }
+  });
+
+  return res.send(JSON.stringify(titleBooks, null, 4));
+});
+
+// Get all books based on title using async-await with Axios
+public_users.get('/asynctitle/:title', async function (req, res) {
+  try {
+    const title = req.params.title;
+    const response = await axios.get(`http://127.0.0.1:5000/title/${title}`);
+    return res.send(JSON.stringify(response.data, null, 4));
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to retrieve title details using Axios' });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(JSON.stringify(books[isbn].reviews, null, 4));
 });
 
 module.exports.general = public_users;
